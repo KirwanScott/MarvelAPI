6dc62e0948e379abe1da82de2b88196d1c3730bb
KirwanScott <102665504+KirwanScott@users.noreply.github.com>
2023-08-05 17:32:47 +0200

    commited

---
+++ app/src/main/java/com/oyelabs/marvel/universe/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.oyelabs.marvel.universe.utils
+
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.sql.Timestamp
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://gateway.marvel.com/"
+        val timeStamp = Timestamp(System.currentTimeMillis()).time.toString()
+        const val API_KEY = "c6ccfe53baae7a3955ce5f8549ff850a"
+        const val PRIVATE_KEY = "523eb7817196be2d1d571f9d679f3821db78a7aa"
+        const val limit = "20"
+        fun hash(): String {
+            val input = "$timeStamp$PRIVATE_KEY$API_KEY"
+            val md = MessageDigest.getInstance("MD5")
+            return BigInteger(1,md.digest(input.toByteArray())).toString(16).padStart(32,'0')
+        }
+    }
+}
